6fc387845af7e8df684e5787dbd6ad47063b9143
LeonardM <LeonardM@hollywoodbets.net>
2023-06-20 22:10:30 +0200

    End of section 5

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,19 +1,27 @@
+import { useEffect, useState } from "react";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
-import { useState, useEffect } from "react";
 
-export default function Catalog(){
+export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/products')
-        .then(response => response.json())
-        .then(data => setProducts(data))
-    }, []) // [] is responsible for calling useEffect once
+        agent.Catalog.list()
+            .then(products => {
+                setProducts(products)
+            })
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+    }, [])
 
-    return(
+    if (loading) return <LoadingComponent message="Loading products..." />
+
+    return (
         <>
             <ProductList products={products} />
-        </>        
+        </>
     )
 }
