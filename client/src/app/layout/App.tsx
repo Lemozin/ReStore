f232946921c737cf58051e698a6d070050a82ae4
LeonardM <LeonardM@hollywoodbets.net>
2023-07-14 13:32:52 +0200

    End of section 7

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,29 +1,30 @@
-import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import Header from "./Header";
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
-import { useStoreContext } from "../context/StoreContext";
+import Header from "./Header";
+import 'react-toastify/dist/ReactToastify.css';
 import { getCookie } from "../util/Util";
 import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
+import { useAppDispatch } from "../store/configureStore";
+import { setBasket } from "../../features/basket/basketSlice";
 
 function App() {
-  const {setBasket} = useStoreContext();
+  const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
 
-  useEffect(() =>{
+  useEffect(() => {
     const buyerId = getCookie('buyerId');
-    if (buyerId){
+    if (buyerId) {
       agent.Basket.get()
-        .then(basket => setBasket(basket))
+        .then(basket => dispatch(setBasket(basket)))
         .catch(error => console.log(error))
         .finally(() => setLoading(false));
     } else {
       setLoading(false);
     }
-  }, [setBasket])
+  }, [dispatch])
 
   const [darkMode, setDarkMode] = useState(false);
   const palleteType = darkMode ? 'dark' : 'light';
@@ -31,25 +32,25 @@ function App() {
     palette: {
       mode: palleteType,
       background: {
-        default: palleteType === 'light' ? '#eaeaea' : '#121212'
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212'
       }
     }
   })
 
-  function handleThemeChange(){
+  function handleThemeChange() {
     setDarkMode(!darkMode);
   }
 
-  if (loading) return <LoadingComponent message="Initializing..." />
-
+  if (loading) return <LoadingComponent message="Initialising app..." />
+  
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <CssBaseline />
-      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}  />
       <Container>
-        <Outlet /> 
-      </Container>      
+        <Outlet />
+      </Container>
     </ThemeProvider>
   );
 }
