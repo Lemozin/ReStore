7cc0824466c16c11d78e9d3cea34bd111549b0a3
LeonardM <LeonardM@hollywoodbets.net>
2023-08-10 15:34:57 +0200

    End of section 9

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,20 +1,26 @@
-import { Navigate, createBrowserRouter } from "react-router-dom";
-import App from "../layout/App";
-import HomePage from "../../features/home/HomePage";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
+import Login from "../../features/account/Login";
+import Register from "../../features/account/Register";
+import BasketPage from "../../features/basket/BasketPage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
-import AboutPage from "../../features/about/AboutPage";
-import ContactPage from "../../features/contact/ContactPage";
-import ServerError from "../errors/ServerError";
-import NotFound from "../errors/NotFound";
-import BasketPage from "../../features/basket/BasketPage";
 import CheckoutPage from "../../features/checkout/CheckoutPage";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
+import NotFound from "../errors/NotFound";
+import ServerError from "../errors/ServerError";
+import App from "../layout/App";
+import RequireAuth from "./RequireAuth";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
         children: [
+            {element: <RequireAuth />, children: [
+                {path: 'checkout', element: <CheckoutPage />},
+            ]},
             {path: '', element: <HomePage />},
             {path: 'catalog', element: <Catalog />},
             {path: 'catalog/:id', element: <ProductDetails />},
@@ -23,8 +29,9 @@ export const router = createBrowserRouter([
             {path: 'server-error', element: <ServerError />},
             {path: 'not-found', element: <NotFound />},
             {path: 'basket', element: <BasketPage />},
-            {path: 'checkout', element: <CheckoutPage />},
-            {path: '*', element: <Navigate replace to='/not-found'/>}
+            {path: 'login', element: <Login />},
+            {path: 'register', element: <Register />},
+            {path: '*', element: <Navigate replace to='/not-found' />}
         ]
     }
 ])
